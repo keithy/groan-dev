2cc0871dfa2748e3f6a6d4791b5ffb2de407be24
keithy <keithy@consultant.com>
2019-09-30 20:53:36 +0100

    Added groan version and tests

---
+++ tester/test_composed_script/test_base_script-version.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+source ../bash-spec-2/bash-spec.sh
+
+describe "Test groan base_script" && {
+
+  context "base_script test --debug output" && {
+    it "Displays debug output" && {
+      capture out <(./base_script version --theme=0)
+        
+      printf '[%s]\n' "${out[@]}"
+ 
+      expect_array out to_contain "(test_composed_script) : *"
+      expect_array out to_contain "(test_composed_script) base_script: *"
+      expect_array out to_contain "(level1) base_script level: *"
+      expect_array out to_contain "(level2) base_script level level: *"
+      expect_array out to_contain "(level3) base_script level level level: *"
+    }
+  }  
+}
+
