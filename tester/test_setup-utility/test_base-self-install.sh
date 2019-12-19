0bcddede4f7b0110e52a1a9c78e91620e2af3cc8
keithy <keithy@consultant.com>
2019-12-19 14:10:43 +0000

    Simplified, upd README, fix themes

--- tester/test_setup-utility/test_base-self-install.sh
+++ tester/test_setup-utility/test_base-self-install.sh
@@ -2,7 +2,7 @@
 
 source ../bash-spec-2/bash-spec.sh
 
-rm -f /usr/local/bin/base
+rm -f $HOME/bin/base
 
 describe "Testin: base setup self-install" && {
 
@@ -43,51 +43,50 @@ describe "Testin: base setup self-install" && {
     }
   }
   
-  context "self-install /usr/local/bin --link" && {
+  context "self-install $HOME/bin --link" && {
    
-    it "Performs self-install  /usr/local/bin --link" && {
+    it "Performs self-install  $HOME/bin --link" && {
    
-      capture out <(./base setup self-install /usr/local/bin --link 2>&1)
+      capture out <(./base setup self-install $HOME/bin --link 2>&1)
       should_succeed
 
       printf '[%s]\n' "${out[@]}"
  
-      expect_array out to_contain "ln -s */tester/test_*/base /usr/local/bin/base"
+      expect_array out to_contain "ln -s */tester/test_*/base $HOME/bin/base"
       expect_array out to_contain "dryrun: --confirm required to proceed"
     }
   }
   
   context "self-install /usr/local/bin --link --confirm" && {
    
-    it "Performs self-install  /usr/local/bin --link --confirm" && {
+    it "Performs self-install  $HOME/bin --link --confirm" && {
    
-      capture out <(./base setup self-install /usr/local/bin --link --confirm 2>&1)
+      capture out <(./base setup self-install $HOME/bin --link --confirm 2>&1)
       should_succeed
 
       printf '[%s]\n' "${out[@]}"
  
-      expect_array out to_contain "ln -s */tester/test_*/base /usr/local/bin/base"
-      expect_array out to_contain "Installed symbolic link from /usr/local/bin/base to */tester/test_*/base"
+      expect_array out to_contain "ln -s */tester/test_*/base $HOME/bin/base"
+      expect_array out to_contain "Installed symbolic link from $HOME/bin/base to */tester/test_*/base"
       expect_array out not to_contain "*base: File exists"
       expect_array out not to_contain "dryrun*"
     }
   }
   
-  context "self-install /usr/local/bin --link --confirm" && {
+  context "self-install $HOME/bin --link --confirm" && {
    
-    it "Performs self-install  /usr/local/bin --link --confirm" && {
+    it "Performs self-install  $HOME/bin --link --confirm" && {
    
-      capture out <(./base setup self-install /usr/local/bin --link --confirm 2>&1)
+      capture out <(./base setup self-install $HOME/bin --link --confirm 2>&1)
       should_succeed
 
       printf '[%s]\n' "${out[@]}"
  
-      expect_array out to_contain "ln -s */tester/test_*/base /usr/local/bin/base"
-      expect_array out to_contain "ln: failed to create symbolic link '/usr/local/bin/base': File exists"
+      expect_array out to_contain "ln -s */tester/test_*/base $HOME/bin/base"
+      expect_array out to_contain "ln: failed to create symbolic link '$HOME/bin/base': File exists"
       expect_array out not to_contain "dryrun*"
-      ln: failed to create symbolic link '/usr/local/bin/base': File exists
     }
   }
 }
 
-rm -f /usr/local/bin/base
+rm -f $HOME/bin/base
